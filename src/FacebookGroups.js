587d7c828b556ec50db4af21d609b78a7a4b9144
stevenjchang <stevenjchang@gmail.com>
2018-09-10 22:19:41 -0700

    add facebook group links

---
+++ src/FacebookGroups.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+const FacebookGroups = () => {
+  return (
+    <div>
+      <h1>Facebook Rental Groups - SF Bay Area</h1>
+      <ol>
+        <li><a href="https://www.facebook.com/groups/786797438080489/"><p>San Francisco Ca✔ For Rent Rooms, Appartments, Houses</p></a></li>
+        <li><a href="https://www.facebook.com/groups/1511607562483357/">San Francisco Bay Area - Roommates, Rooms, Apartments, Rentals, Sublets</a></li>
+        <li><a href="https://www.facebook.com/groups/945920215452910/">Affordable Housing For Ok People: Bay Area</a></li>
+        <li><a href="https://www.facebook.com/groups/317688158367767/">Friends looking for SF housing! -Anjou</a></li>
+        <li><a href="https://www.facebook.com/groups/390478684333910/">Bay Area Rooms and Apartments</a></li>
+        <li><a href="https://www.facebook.com/groups/303241339725481/">Bay Area Conscious Community Housing Board</a></li>
+        <li><a href="https://www.facebook.com/groups/492571644257934/">San Francisco Bay Area group to find rooms for rent and roommates</a></li>
+        <li><a href="https://www.facebook.com/groups/843764532374203/">San Francisco Housing, Rooms, Apartments, Sublets</a></li>
+        <li><a href="https://www.facebook.com/groups/sfhousing/">San Francisco, SF - Housing, Apartments, Rooms, Sublets, Roommates</a></li>
+        <li><a href="https://www.facebook.com/groups/1041652969189404/">Bay Area Rooms, Apartments and House Rentals</a></li>
+        <li><a href="https://www.facebook.com/groups/findyournextopportunity/">Albert's Job Listings & Referrals - San Francisco Bay Area</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ol>
+    </div>
+  )
+}
+
+export default FacebookGroups;
